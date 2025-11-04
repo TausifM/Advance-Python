f13cc71e7085b8eb930e0c90aeead9c514ad0419
TausifM <heytausifsheikh@gmail.com>
2025-11-04 13:03:22 +0530

    class 2

--- class1.py
+++ class1.py
@@ -2,10 +2,10 @@
 a,b,c = [1,2,3]
 print(f"a: {a}, b: {b}, c: {c}") # a: 1, b: 2, c: 3
 
-# Extended unpacking Iterables
+# Extended unpacking Iterables * means asterisk
 a, *b = [1,2,3,4,5, 6, 7, 8, 9, 10 ]
 print(f"a: {a}, b: {b}, c: {c}") # a: 1, b: [2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # Ignoring Values
 _, _, c = [1,2,3]
-print(f"_" , f"_: {_}, c: {c}") # _: 2, c: 3
+print(f"_: {_}", f"_: {_}, c: {c}") # _: 2 _: 2, c: 3
---
+++ class2.py
@@ -0,0 +1,42 @@
+# Unpacking Nested Structures
+data = ("Alice", (25, "Engineer"), ["Reading", "Traveling", "Swimming"])
+name, (age, profession), [hobbies, travel, swimming] = data
+# hobbies[0] = data[0] # Alice
+# print(hobbies[2]) # Swimming
+hobbies = data[0] # Alice
+print(f"Name: {name}, Age: {age}, Profession: {profession}, Hobbies: {hobbies}, Travel: {travel}, Swimming: {swimming}")
+# Name: Alice, Age: 25, Profession: Engineer, Hobbies: Alice, Travel: Swimming, Swimming: Swimming
+
+# Unpacking with function arguments
+def print_names(*names):
+    for name in names:
+        print(f"Name: {name}")
+        
+print_names(["Bob", 25], ["Charlie", 30], ["David", 35])
+
+# Name: ['Bob', 25]
+# Name: ['Charlie', 30]
+# Name: ['David', 35]
+
+# Combining Lists with Unpacking
+list1 = [1, 2, 3, 7, 8, 9]
+list2 = [4, 5, 6]
+combined_list = [*list1, *list2]
+print(*list1)  # 1 2 3 7 8 9
+sorted_combined = sorted(combined_list)
+
+print(f"Combined List: {combined_list}") # Combined List: [1, 2, 3, 7, 8, 9, 4, 5, 6]
+print(f"Sorted Combined List: {sorted_combined}") # Sorted Combined List: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Merging Dictionaries with Unpacking 
+dict1 = {'x': 1, 'y': 2}
+dict2 = {'y': 3, 'z': 4}
+merged_dict = {**dict1, **dict2}
+print(f"Merged Dictionary: {merged_dict}") # Merged Dictionary: {'x': 1, 'y': 3, 'z': 4}
+# Note: In case of key conflicts, the value from the latter dictionary is used.
+
+# Swapping Variables
+x = 10
+y = 20
+x, y = y, x
+print(f"x: {x}, y: {y}") # x: 20, y: 10
