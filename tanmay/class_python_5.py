85c3c4366b3874cbc1852bd4fd48e7f04d11ac7f
TausifM <heytausifsheikh@gmail.com>
2025-11-06 13:14:29 +0530

    class method changes

--- tanmay/class_python_5.py
+++ tanmay/class_python_5.py
@@ -3,13 +3,18 @@ class Tanmay:
     def __init__(self, name, aadhar):
         self.name = name
         self.aadhar = aadhar
+        print(f"name: {self.name}, aadhar: {self.aadhar}")
 
     def validation(self):
-        if len(self.aadhar) ==12 and self.aadhar.is_digit():
+        if len(str(self.aadhar)) == 12 and str(self.aadhar).isdigit():
             return f"aadhar is valid"
         else:
             return f"aadhar is invalid"
         
-print(Tanmay("name",1234))
-print(Tanmay.validation())
+print(Tanmay("name",1234).validation()) 
+# to solve below error we used str() function
+# TypeError: object of type 'int' has no len()
+print(Tanmay("tanmay", "123456789012").validation())
+# Thats why we are passing aadhar as string
+#print(Tanmay("tanmay", "12345678901").validation())
         
