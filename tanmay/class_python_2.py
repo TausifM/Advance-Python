ba764155876cb3aa92a6061fef8fbcaebb7d6f6d
ttanmayyy26 <tanmayagrawwal@gmail.com>
2025-11-08 13:17:14 +0530

    list

--- tanmay/class_python_2.py
+++ tanmay/class_python_2.py
@@ -1,20 +1,20 @@
 data= ("Tanmay", (21, "Male"), ["Reading", "Traveling", "Coding"])
 name, (age, gender), hobbies = data
-hobbies = data[2]
+hobbies = data[0]
 print(f"Name: {name}, age: {age}, Gender: {gender}, Hobbies: {hobbies}")
-hobbies[1] = data[2]
 
 def print_info(*names):
     for name in names:
         print(f"Hello, {name}")
 print_info("Tanmay", "Sanskruti", "Omkar")
 
-list1 = [2,5,1,7,3]
+list1 = [2,5,5,1,7,3]
 list2 = [4,8,0,9,6]
 combined = [*list1, *list2]
 print(combined)
 sorted_combined = sorted(combined)
 print(sorted_combined)
+print(list(set(sorted_combined)))
 
 dict1 = {'a': "Thor", 'b': "Loki"}
 dict2 = {'c': "Odin", 'd': "Ironman"}
---
+++ tanmay/class_python_4.py
@@ -0,0 +1,5 @@
+name : str= " Tanmay"
+age : int = 21
+is_student : bool = True
+def greet(x: str, age:int) -> str:
+    return f"Hello, my name is {x} and I am {age} years old."
