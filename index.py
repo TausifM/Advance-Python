d4f5e500f7680d0afb2f6aa587ab34ca36bca6e1
TausifM <heytausifsheikh@gmail.com>
2025-11-26 13:21:32 +0530

    index.py api start

---
+++ index.py
@@ -0,0 +1,13 @@
+# make an api listerner route port 5000 using FastAPI
+
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=5000)
+    
+# to run the app use the command: uvicorn index:app --host 0.0.0.0 --port 5000
